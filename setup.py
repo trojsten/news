d67431ba465806d15973d44f675226cebb5bbd2f
Michal Hozza <mhozza@gmail.com>
2016-06-06 10:32:38 +0200

    explicit version in setup.py

--- setup.py
+++ setup.py
@@ -1,25 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import os
-import re
 import sys
 
 try:
     from setuptools import setup
 except ImportError:
     from distutils.core import setup
-
-
-def get_version(*file_paths):
-    filename = os.path.join(os.path.dirname(__file__), *file_paths)
-    version_file = open(filename).read()
-    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
-                              version_file, re.M)
-    if version_match:
-        return version_match.group(1)
-    raise RuntimeError('Unable to find version string.')
-
-version = get_version('news', '__init__.py')
 
 if sys.argv[-1] == 'publish':
     try:
@@ -32,18 +19,12 @@ if sys.argv[-1] == 'publish':
     os.system('python setup.py bdist_wheel upload')
     sys.exit()
 
-if sys.argv[-1] == 'tag':
-    print("Tagging the version on github:")
-    os.system("git tag -a %s -m 'version %s'" % (version, version))
-    os.system("git push --tags")
-    sys.exit()
-
 readme = open('README.rst').read()
 history = open('HISTORY.rst').read().replace('.. :changelog:', '')
 
 setup(
     name='django-trojsten-news',
-    version=version,
+    version='0.1.0',
     description="""News for django, used in Trojsten seminary web apps""",
     long_description=readme + '\n\n' + history,
     author='Michal Hozza',
