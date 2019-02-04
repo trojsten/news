2b937a33304b1f21e6c3f79b6bb5c37f595f1b93
Michal Hozza <mhozza@gmail.com>
2019-02-04 22:22:57 +0000

    More Django 2 fixes.

--- example/example/urls.py
+++ example/example/urls.py
@@ -6,9 +6,7 @@ from django.views.generic.base import RedirectView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^accounts/login/$', auth_views.LoginView.as_view(), {
-        'template_name': 'admin/login.html'
-    }),
+    url(r'^accounts/login/$', auth_views.LoginView.as_view(template_name='admin/login.html')),
     url('^accounts/', include('django.contrib.auth.urls')),
     url(r'', include('news.urls')),
     url(r'^$', RedirectView.as_view(
