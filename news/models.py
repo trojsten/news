ec5753429da8cefada38c8ed6d403824a9ca5bfe
David Barbora <david.barbora@vacuumlabs.com>
2021-12-23 22:53:51 +0100

    add compatibility with Django 3

--- news/models.py
+++ news/models.py
@@ -5,7 +5,7 @@ from autoslug import AutoSlugField
 from django.conf import settings
 from django.contrib.sites.models import Site
 from django.db import models
-from django.utils.encoding import python_2_unicode_compatible
+from six import python_2_unicode_compatible
 from django.utils.html import mark_safe
 from markdown import markdown
 from taggit.managers import TaggableManager
